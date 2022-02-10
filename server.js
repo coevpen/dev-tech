e619c025dba1f36bffbd9711681b176335fa5107
coevpen <koinuevins@gmail.com>
2022-02-09 18:34:04 -0600

    created api routes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sesh = {
     secret: 'Super secret secret',
